1b4a739c8865c2664fc4cc4d4b8345866764ebe6
paulo <paulosilvarl17@gmail.com>
2025-05-21 09:57:23 -0300

    Separei o head e header em parciais HTML; ajustei o base.html e contact.html para usar includes; e atualizei o contact_views.py

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -11,6 +11,7 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Lista de Contatos - ',
     }
 
     return render(request, 'contact/index.html', context)
@@ -19,8 +20,11 @@ def contact(request, contact_id):
     
     single_contact = get_object_or_404(Contact, id=contact_id, show=True)
 
+    contact_name = f'{single_contact.first_name} {single_contact.last_name} - ' 
+
     context = {
         'contact': single_contact,
+        'site_title': contact_name,
     }
 
     return render(request, 'contact/contact.html', context)
